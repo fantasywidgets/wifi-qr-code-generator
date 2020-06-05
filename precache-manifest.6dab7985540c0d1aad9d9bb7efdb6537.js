0690816930f92910e8994111e1e549cf7127f493
Anoop Engineer <anoopengineer@gmail.com>
2020-06-05 09:23:24 -0500

    Updates

---
+++ precache-manifest.6dab7985540c0d1aad9d9bb7efdb6537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f869639b2151f7e399c79e5dd9e9db3a",
+    "url": "/wifi-qr-code-generator/index.html"
+  },
+  {
+    "revision": "9cd13b607a4f5bbebe8a",
+    "url": "/wifi-qr-code-generator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e800f342c5e6e9fc80b6",
+    "url": "/wifi-qr-code-generator/static/js/2.8af8b9b3.chunk.js"
+  },
+  {
+    "revision": "2671755c56578721a6e26bb7a786e558",
+    "url": "/wifi-qr-code-generator/static/js/2.8af8b9b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd13b607a4f5bbebe8a",
+    "url": "/wifi-qr-code-generator/static/js/main.cd77b9b6.chunk.js"
+  },
+  {
+    "revision": "b517d6701bc957de77f9",
+    "url": "/wifi-qr-code-generator/static/js/runtime-main.a667ba9f.js"
+  }
+]);
